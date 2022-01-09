2bebcaa5e854a505e103114e01bc26ff48452134
Jim Lougheed <jims.codes@gmail.com>
2022-01-08 22:52:49 -0500

    Add filterPokemon variable and map encounter details

--- client/src/components/RouteInfo/RouteInfo.jsx
+++ client/src/components/RouteInfo/RouteInfo.jsx
@@ -12,21 +12,61 @@ function RouteInfo({ route }) {
     setPokemon(data.pokemon_encounters);
   };
 
+  const filteredPokemon =
+    pokemon &&
+    pokemon.filter((pokemon) => {
+      return pokemon.version_details.some((detail) => {
+        return detail.version.name === "pearl";
+      });
+    });
+  console.log(filteredPokemon);
+
   return (
     <>
       <h1>Route Info</h1>
       <p>{route}</p>
       <ul>
-        {pokemon &&
-          pokemon.map((pokemon) => {
+        {filteredPokemon &&
+          filteredPokemon.map((pokemon) => {
             console.log(pokemon);
-            {
-              /* if (pokemon.version_details.version.name === "pearl") { */
-            }
-            return <li>{pokemon.pokemon.name}</li>;
-            {
-              /* } */
-            }
+            return (
+              <li>
+                <h3>{pokemon.pokemon.name}</h3>
+                <ul>
+                  {pokemon.version_details[1]
+                    ? pokemon.version_details[1].encounter_details.map(
+                        (detail) => {
+                          return (
+                            <li>
+                              <p>Method: {detail.method.name}</p>
+                              <p>
+                                Condition:
+                                {detail.condition_values[0] != undefined
+                                  ? detail.condition_values[0].name
+                                  : null}
+                              </p>
+                            </li>
+                          );
+                        }
+                      )
+                    : pokemon.version_details[0].encounter_details.map(
+                        (detail) => {
+                          return (
+                            <li>
+                              <p>Method: {detail.method.name}</p>
+                              <p>
+                                Condition:{" "}
+                                {detail.condition_values[0] != undefined
+                                  ? detail.condition_values[0].name
+                                  : null}
+                              </p>
+                            </li>
+                          );
+                        }
+                      )}
+                </ul>
+              </li>
+            );
           })}
       </ul>
     </>
