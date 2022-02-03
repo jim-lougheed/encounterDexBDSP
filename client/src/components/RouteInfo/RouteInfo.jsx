113e9f2083ee4e476a1a740e940879e636baa46d
Jim Lougheed <jims.codes@gmail.com>
2022-02-02 21:57:24 -0500

    COmplete styling of header and banner

--- client/src/components/RouteInfo/RouteInfo.jsx
+++ client/src/components/RouteInfo/RouteInfo.jsx
@@ -4,6 +4,17 @@ import axios from "axios";
 import PokemonListing from "../PokemonListing/PokemonListing";
 import Selection from "../Selection/Selection";
 
+import Piplup from "../../assets/images/piplup.png";
+import Chimchar from "../../assets/images/chimchar.png";
+import Turtwig from "../../assets/images/turtwig-flip.png";
+import Palkia from "../../assets/images/palkia-flip.png";
+import Dialga from "../../assets/images/dialga.png";
+import Houndoom from "../../assets/images/houndoom.png";
+import Girafarig from "../../assets/images/girafarig-flip.png";
+import Crobat from "../../assets/images/crobat.png";
+import Rhydon from "../../assets/images/rhydon.png";
+import Luxray from "../../assets/images/luxray-flip.png";
+
 import "./RouteInfo.scss";
 
 function RouteInfo({ route, routeName, setRoute, setRouteName }) {
@@ -28,11 +39,23 @@ function RouteInfo({ route, routeName, setRoute, setRouteName }) {
 
   return (
     <>
-      <div className="route__header">
-        <h1 className="route__title">
-          {routeName ? routeName : "Select an area"}
-        </h1>
-        <Selection setRoute={setRoute} setRouteName={setRouteName} />
+      <div className="route__container">
+        <img className="banner-pokemon banner-pokemon--1" src={Turtwig} />
+        <img className="banner-pokemon banner-pokemon--2" src={Girafarig} />
+        <img className="banner-pokemon banner-pokemon--3" src={Houndoom} />
+        <img className="banner-pokemon banner-pokemon--4" src={Luxray} />
+        <img className="banner-pokemon banner-pokemon--5" src={Dialga} />
+        <img className="banner-pokemon banner-pokemon--6" src={Palkia} />
+        <img className="banner-pokemon banner-pokemon--7" src={Chimchar} />
+        <img className="banner-pokemon banner-pokemon--8" src={Rhydon} />
+        <img className="banner-pokemon banner-pokemon--9" src={Crobat} />
+        <img className="banner-pokemon banner-pokemon--10" src={Piplup} />
+        <div className="route__header">
+          <h1 className="route__title">
+            {routeName ? routeName : "Select an area"}
+          </h1>
+          <Selection setRoute={setRoute} setRouteName={setRouteName} />
+        </div>
       </div>
 
       <PokemonListing pokemon={filteredPokemon} />
